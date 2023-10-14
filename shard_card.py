b248f8615d42162d8c2c7923382af8c82f6ed674
Gaurav Sablok <sablokg@gmail.com>
2023-10-14 22:18:45 +0530

    Update shard_card.py

--- shard_card.py
+++ shard_card.py
@@ -15,4 +15,10 @@ def shard_card(url):
                                        [j.split()[2] for i in shard_store for j in i if "shard-name" in j]
     shard_description = [i.split("\n") for i in (list(map(str,shard_desc)))]
     shard_description_final = list(filter(None,(map(''.join,[re.findall(r'[A-Za-z]{0,}',i) for i in [i for i in (j for i in shard_description for j in i if "description" not in j) if "div" not in i]]))))
-    return shard_desc
+    return shard_des
+    # some of the gits dont have the description hence you cant get a dataframe out of it. 
+    # if they allow to add then you can add the following line and return layout and it will give you a complete indexed layout of the shards
+    #shard_info_layout = {}
+    # for i in range(len(shard_name)):
+    #   shard_info_layout[shard_name[i]] = [[shard_des[i], shard_description_final[i]]]
+    # return shard_info_layout

5017854fb264b519e7709092d69cb111929f889a
Gaurav Sablok <sablokg@gmail.com>
2023-10-14 21:54:02 +0530

    Update shard_card.py

--- shard_card.py
+++ shard_card.py
@@ -8,11 +8,11 @@ def shard_card(url):
     from bs4 import BeautifulSoup
     url_fetch =  urlopen(url)
     shard_card = BeautifulSoup(url_fetch, "html.parser").find_all("div", class_="shard-card")
-    #url_fetch =  urlopen(url)
+    url_fetch =  urlopen(url)
     shard_desc = BeautifulSoup(url_fetch, "html.parser").find_all("div", class_="description")
     shard_store = [i for i in (i.split("\n") for i in list(map(str,shard_card))) for j in i if "shard-name" in j]
     shard_name, shard_des = [j.split()[1] for i in shard_store for j in i if "shard-name" in j],\
                                        [j.split()[2] for i in shard_store for j in i if "shard-name" in j]
     shard_description = [i.split("\n") for i in (list(map(str,shard_desc)))]
     shard_description_final = list(filter(None,(map(''.join,[re.findall(r'[A-Za-z]{0,}',i) for i in [i for i in (j for i in shard_description for j in i if "description" not in j) if "div" not in i]]))))
-    return shard_des
+    return shard_desc
